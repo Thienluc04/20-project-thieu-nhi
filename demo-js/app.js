df3afab4c103d468db3235dd711fe85c782a8dd5
Luc <nguyenthienluc2004@gmail.com>
2021-12-06 08:17:13 +0700

    update code

--- demo-js/app.js
+++ demo-js/app.js
@@ -1,5 +1,5 @@
 
-var courseAPI = 'http://localhost:3000/courses'
+var courseAPI = 'http://localhost:3000/product'
 
 
 function start() {
@@ -18,15 +18,17 @@ function getCourses(callback) {
         .then(callback);
 }
 
-function renderCourses(courses) {
-    var listCourseBlock = document.getElementById('list-course')
+function renderCourses(products) {
+    var listCourseBlock = document.getElementById('list-product')
 
-    var htmls = courses.map(function (course) {
+    var htmls = products.map(function (course) {
         return `<li class="course-item-${course.id}">
                     <h1 class="course-name-${course.id}">${course.name}</h1>
                     <h3 class="course-description-${course.id}">${course.description}</h3>
+                    <img class="course-image-${course.id}" src="${course.image}" alt=""><br/>
                     <button onclick="handleDeleteCourse(${course.id})">Xóa</button>
                     <button onclick="handleUpdateCourse(${course.id})">Sửa</button>
+                    
                 </li>`;
     });
     listCourseBlock.innerHTML = htmls.join('')
@@ -71,12 +73,15 @@ function handleDeleteCourse(id) {
 function handleUpdateCourse(id) {
     var nameInput = document.querySelector('input[name="name"]')
     var descriptionInput = document.querySelector('input[name="description"]')
+    var imgInput = document.querySelector('input[name="image"]')
 
     var courseName = document.querySelector('.course-name-' + id).innerText
     var courseDescription = document.querySelector('.course-description-' + id).innerText
+    var courseImg = document.querySelector('.course-image-' + id).src
 
     courseName = nameInput.value
     courseDescription = descriptionInput.value
+    courseImg = imgInput.value
 
     var updateBtn = document.querySelector('.update')
     updateBtn.innerText = "SAVE"
@@ -84,11 +89,13 @@ function handleUpdateCourse(id) {
     updateBtn.onclick = function () {
         var updateData = {
             name: nameInput.value,
-            description: descriptionInput.value
+            description: descriptionInput.value,
+            image: imgInput.value
         }
         updateBtn.innerText = "Create"
         nameInput.value = ""
         descriptionInput.value = ""
+        imgInput.value = ""
 
         var options= {
             method: 'PUT',
@@ -116,16 +123,20 @@ function handleCreateForm() {
     createBtn.onclick = function() {
         var name = document.querySelector('input[name="name"]').value
         var description = document.querySelector('input[name="description"]').value
+        var image = document.querySelector('input[name="image"]').value
         
         var nameInput = document.querySelector('input[name="name"]')
         var descriptionInput = document.querySelector('input[name="description"]')
+        var imageInput = document.querySelector('input[name="image"]')
 
         nameInput.value = ""
         descriptionInput.value = ""
+        imageInput.value = ""
         
         var formData = {
             name: name,
             description: description,
+            image: image,
         }
 
         createCourse(formData, function() {
